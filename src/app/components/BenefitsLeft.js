856b3cfa583bcf4209121bcd72ca105662927076
Jorge Andres Ocaris Acuna <jocaris@addi.com>
2023-10-29 12:59:00 -0500

    Fixing some accesibility issues 2

--- src/app/components/BenefitsLeft.js
+++ src/app/components/BenefitsLeft.js
@@ -17,9 +17,9 @@ export const BenefitsLeft = () => {
 
         <div className={'flex flex-wrap items-center w-full lg:w-1/2'}>
           <div className="flex flex-col w-full mt-4 justify-center items-center">
-            <h3 className="max-w-2xl text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl text-center">
+            <h1 className="max-w-2xl text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl text-center">
               The time of your colleagues matters
-            </h3>
+            </h1>
 
             <p className="font-sans py-2 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl text-center">
               How many times have you asked a coworker about a decision or outcome just because it was faster than wandering around the docs looking for the answer?
--- src/app/components/Features.js
+++ src/app/components/Features.js
@@ -3,9 +3,9 @@ export const Features = () => {
     <div className="mx-auto container mt-8 pl-20 pr-20 pt-8 grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-16 ">
       <article className="flex max-w-xl flex-col items-start justify-between">
         <div className="group relative">
-          <h3 className="mt-3 text-2xl font-semibold leading-6 text-gray-800 group-hover:text-gray-500 text-center">
+          <h1 className="mt-3 text-2xl font-semibold leading-6 text-gray-800 group-hover:text-gray-500 text-center">
             Use it right where you need it
-          </h3>
+          </h1>
           <p className="mt-5 text-lg leading-6 text-gray-500 text-center">
             Integrated with Slack, and more tools are on the way (Microsoft
             Teams, Discord, Whatsapp and more).
@@ -15,9 +15,9 @@ export const Features = () => {
 
       <article className="flex max-w-xl flex-col items-start justify-between">
         <div className="group relative">
-          <h3 className="mt-3 text-2xl font-semibold text-gray-800 group-hover:text-gray-500 text-center">
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 group-hover:text-gray-500 text-center">
             Connect to your Notion docs
-          </h3>
+          </h1>
           <p className="mt-5 text-lg leading-6 text-gray-500 text-center">
             We support Notion as a data source with just a few clicks. More data
             sources will be added in the future.
@@ -27,9 +27,9 @@ export const Features = () => {
 
       <article className="flex max-w-xl flex-col items-start justify-between">
         <div className="group relative">
-          <h3 className="mt-3 text-2xl font-semibold leading-6 text-gray-800 group-hover:text-gray-500 text-center">
+          <h1 className="mt-3 text-2xl font-semibold leading-6 text-gray-800 group-hover:text-gray-500 text-center">
             You own your data, we secure it
-          </h3>
+          </h1>
           <p className="mt-5 text-lg leading-6 text-gray-500 text-center">
             We do not use your data to train models. Rest assured, your data is
             safe; we encrypt everything, everywhere.
