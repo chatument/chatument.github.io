00ea770f13111d39c073f1495f5b9b674db856df
Jorge Andres Ocaris Acuna <jocaris@addi.com>
2023-10-29 12:44:56 -0500

    Fixing some issues

--- src/app/components/Faq.js
+++ src/app/components/Faq.js
@@ -17,40 +17,43 @@ const questions = [
 
 export const Faq = () => {
   return (
-    <ul className="max-w-xl mx-auto divide-y mt-16 lg:mt-20">
-      <div>
+    <>
+      <div className="flex flex-col justify-center items-center mt-16 ">
         <h2 className="max-w-2xl mb-8 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl text-center ">
           FAQ
         </h2>
       </div>
-      {questions.map(({ question, aswer }) => {
-        return (
-          <li className="mb-2" key={question}>
-            <details className="group">
-              <summary className="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
-                <svg
-                  className="w-5 h-5 text-gray-500 transition group-open:rotate-90"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                  ></path>
-                </svg>
-                <span>{question}</span>
-              </summary>
 
-              <article className="px-4 pb-4">
-                <p className="font-sans text-gray-500">{aswer}</p>
-              </article>
-            </details>
-          </li>
-        );
-      })}
-    </ul>
+      <ul className="max-w-xl mx-auto divide-y lg:mt-20">
+        {questions.map(({ question, aswer }) => {
+          return (
+            <li className="mb-2" key={question}>
+              <details className="group">
+                <summary className="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
+                  <svg
+                    className="w-5 h-5 text-gray-500 transition group-open:rotate-90"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                  <span>{question}</span>
+                </summary>
+
+                <article className="px-4 pb-4">
+                  <p className="font-sans text-gray-500">{aswer}</p>
+                </article>
+              </details>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,16 +1,14 @@
-import Link from "next/link";
-import { Logo } from "./Logo";
+import Link from 'next/link';
+import { Logo } from './Logo';
 
 export const Footer = () => {
   return (
     <footer>
       <div className="mx-auto container p-4 py-6 lg:py-8">
-      <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="#" className="flex items-center">
-              <Logo />
-            </a>
+            <Logo />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
@@ -27,11 +25,7 @@ export const Footer = () => {
                   </a>
                 </li>
                 <li>
-                  <a
-                    href="#"
-                    className="hover:underline"
-                  >
-                  </a>
+                  <a href="#" className="hover:underline"></a>
                 </li>
               </ul>
             </div>
@@ -54,14 +48,9 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        {/* <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" /> */}
         <div className="sm:flex sm:items-center sm:justify-between mt-6">
           <span className="text-sm text-gray-500 sm:text-center">
-            © 2023{' '}
-            <a className="hover:underline">
-              Chatument™
-            </a>
-            . All Rights Reserved.
+            © 2023 <p>Chatument™</p>. All Rights Reserved.
           </span>
         </div>
       </div>
