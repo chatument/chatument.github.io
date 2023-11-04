707c01ea74adb0e4c3c6678fbd65c53aa0e9df97
Jorge Andres Ocaris Acuna <jocaris@addi.com>
2023-11-04 07:53:56 -0500

    fix: right animation

--- src/app/components/RightAnimation.js
+++ src/app/components/RightAnimation.js
@@ -10,26 +10,26 @@ export const RigthAnimation = () => {
       />
       <BubbleV2
         text={`You can spin up the local env with just two steps:`}
-        // className="fade-animation delay-500ms"
-        // childClassName='delay-child-500ms'
+        className="fade-animation delay-500ms"
+        childClassName='delay-child-500ms'
         position="left"
       />
       <BubbleV2
         text={`1. Clone the git repositories and set the environment variable \n 2. Run the Docker container with Docker Compose`}
-        // className="fade-animation delay-1000ms"
-        // childClassName='delay-child-1000ms'
+        className="fade-animation delay-1000ms"
+        childClassName='delay-child-1000ms'
         position="left"
       />
       <BubbleV2
         text={`Get more details byfollowing the referenced docs below`}
-        // className="fade-animation delay-1000ms"
-        // childClassName='delay-child-1000ms'
+        className="fade-animation delay-1750ms"
+        childClassName='delay-child-1750ms'
         position="left"
       />
       <BubbleV2
         text={`Got you, thanks my friend!`}
-        // className="fade-animation delay-1000ms"
-        // childClassName='delay-child-1000ms'
+        className="fade-animation delay-2250ms"
+        childClassName='delay-child-2250ms'
         position="right"
       />
     </div>
