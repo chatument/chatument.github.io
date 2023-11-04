7b31d4369bad85d71230e25582c353e633fa5f46
Jorge Andres Ocaris Acuna <jocaris@addi.com>
2023-11-04 12:19:00 -0500

    fix: title width

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -8,7 +8,7 @@ export const Landing = () => {
     <div className="flex container flex-wrap pl-8 pt-8 pr-8 mx-auto sm:pt-24 ">
       <div className="flex items-center w-full lg:w-1/2 md:pr-4">
         <div className="max-w-2xl mb-8">
-          <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight">
+          <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-5xl xl:leading-tight">
             Stop wasting time: Consume and explore your org docs efficiently
           </h1>
           <p className="py-5 font-sans text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl ">
